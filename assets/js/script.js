5b668cbfdf9aac65d67b4b33278331b11fc83566
yomiodusi <99558274+yomiodusi@users.noreply.github.com>
2022-03-27 19:09:11 -0400

    i am stuck

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,11 @@
 var todayDate = moment().format('LLLL');
 $("#currentDay").html(todayDate);
 
+// anycode inside here is ready to be executed. So this is the selector for my button
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+      console.log("click")
+    })
+})
+
+// function to store variable when clicked
